35abce502a308d4faad52df822b3297a8644e362
moriz <william.dedominico88@gmail.com>
2021-11-05 16:54:35 -0500

    init

---
+++ src/main/java/com/BrotherHoodOfDiethylamide/OrangeSunshine/OrangeSunshine.java
@@ -0,0 +1,31 @@
+package com.BrotherHoodOfDiethylamide.OrangeSunshine;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = OrangeSunshine.MODID, name = OrangeSunshine.NAME, version = OrangeSunshine.VERSION)
+public class OrangeSunshine
+{
+    public static final String MODID = "orangesunshine";
+    public static final String NAME = "OrangeSunshine";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
